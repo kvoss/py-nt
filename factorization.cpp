c58f1b935904269a4c346830430598c01a52d7df
K. Voss <k.voss@usask.ca>
2015-11-21 15:38:02 -0600

    separates C++

---
+++ factorization.cpp
@@ -0,0 +1,27 @@
+#include <cmath>
+#include <vector>
+
+#include "Python.h"
+#include "factorization.hpp"
+
+VU
+factor(UL n)
+{
+    VU factors;
+
+    UL limit = sqrtl(n);
+    for (UL x = 2;; ++x) {
+        if (x > limit) break;
+
+        while (n % x == 0) {
+            n /= x;
+            limit = sqrtl(n);
+            factors.push_back(x);
+        }
+    }
+    if (n > 1)
+        factors.push_back(n);
+
+    return factors;
+}
+
---
+++ factorization.hpp
@@ -0,0 +1,10 @@
+#ifndef _FACTORIZATION_HPP
+#define _FACTORIZATION_HPP
+
+typedef unsigned long UL;
+typedef std::vector<UL> VU;
+
+VU factor(UL);
+
+#endif /* _FACTORIZATION_HPP */
+
