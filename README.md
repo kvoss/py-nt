8022963e2cc7a401c34385b6344bdcf844ffd286
K. Voss <shobbo@gmail.com>
2016-10-25 01:42:39 -0600

    an example

--- README.md
+++ README.md
@@ -1,1 +1,17 @@
 # py-nt
+
+The prime numbers have only 1 factor, the secondary numbers have 2 factors, and so on.
+Get distribution of number of factors for a given range of numbers.
+
+For example
+
+    $ python nnd.py
+    ---- NthNumbersDistribution(2093)
+    ----
+    numbers with 8 factors: [256, 384, 576, 640, 864, 896, 960, 1296, 1344, 1408, 1440, 1600, 1664, 1944, 2016]
+    factorizations of len 8: [[2, 2, 2, 2, 2, 2, 2, 2], [2, 2, 2, 2, 2, 2, 2, 3], [2, 2, 2, 2, 2, 2, 3, 3], [2, 2, 2, 2, 2, 2, 2, 5], [2, 2, 2, 2, 2, 3, 3, 3], [2, 2, 2, 2, 2, 2, 2, 7], [2, 2, 2, 2, 2, 2, 3, 5], [2, 2, 2, 2, 3, 3, 3, 3], [2, 2, 2, 2, 2, 2, 3, 7], [2, 2, 2, 2, 2, 2, 2, 11], [2, 2, 2, 2, 2, 3, 3, 5], [2, 2, 2, 2, 2, 2, 5, 5], [2, 2, 2, 2, 2, 2, 2, 13], [2, 2, 2, 3, 3, 3, 3, 3], [2, 2, 2, 2, 2, 3, 3, 7]]
+    lentgh of the longest factorization: 11
+    the longest factorization: [[2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2]]
+    distribution of factorization lengths:  {0: 2, 1: 316, 2: 598, 3: 527, 4: 331, 5: 172, 6: 84, 7: 38, 8: 15, 9: 7, 10: 2, 11: 1}
+    ----
+
--- nnd.py
+++ nnd.py
@@ -4,19 +4,45 @@ import factors as fs
 
 
 class NthNumbersDistribution(object):
+    """
+    TODO: memoize, persist
+    """
     def __init__(self, n):
+        """
+        ..note:: does not include the number n
+        """
         self.ffs = [fs.factor(x) for x in xrange(n)]
 
     def max_factorization_len(self):
+        """
+        >>> max_factorization_len([[2], [3], [2, 2], [5], [2, 3]])
+        2
+        """
         max_factorization_len = max(len(fs) for fs in self.ffs)
         return max_factorization_len
 
     def longest_factorizations(self):
+        """
+        >>> longest_factorizations([[2], [3], [2, 2], [5], [2, 3]])
+        [[2, 2], [2, 3]]
+        """
+        max_len = self.max_factorization_len()
         lfs = list(fs for fs in self.ffs
-                      if len(fs) == self.max_factorization_len())
+                      if len(fs) == max_len)
         return lfs
 
     def nth_nr_dist(self):
+        """
+        how many number do we have that have 1 factor, 2 factors, and so on
+
+        for [[2], [3], [2, 2], [5], [2, 3]] we have
+        0: **
+        1: ***
+        2: **
+        0 and 1 have 0 factors
+        there are 3 prime numbers in
+
+        """
         cntr = defaultdict(int)
         lffs = map(len, self.ffs)
         for l in lffs:
@@ -24,6 +50,9 @@ class NthNumbersDistribution(object):
         return cntr
 
     def nth_nrs(self, nth):
+        """
+        what numbers have exactly n factors?
+        """
         nnrs = []
         for fs in self.nth_factors(nth):
             product = reduce(lambda a, b: a * b, fs, 1)
@@ -31,6 +60,22 @@ class NthNumbersDistribution(object):
         return nnrs
 
     def nth_factors(self, nth):
+        """
+        what factorizations of lenght n do we have
+        """
         nfs = [fs for fs in self.ffs if len(fs) == nth]
         return nfs
+
+if __name__ == '__main__':
+    nn = NthNumbersDistribution(2093)
+    print '---- NthNumbersDistribution(2093)'
+    print '----'
+
+    print 'numbers with 8 factors:', nn.nth_nrs(8)
+    print 'factorizations of len 8:', nn.nth_factors(8)
+    print 'lentgh of the longest factorization:', nn.max_factorization_len()
+    print 'the longest factorization:', nn.longest_factorizations()
+    print 'distribution of factorization lengths: ', dict(nn.nth_nr_dist())
+
+    print '----'
 
